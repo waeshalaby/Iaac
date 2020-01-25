16344d6af047c24a70fa9e16f943436e01128417
wael <Wael.Shalaby@Vodafone.com>
2020-01-25 16:25:57 +0200

    add git install

--- Jenkins/installation_sripts/installJenkins.sh
+++ Jenkins/installation_sripts/installJenkins.sh
@@ -2,7 +2,7 @@
 sudo yum -y install java-1.8.0-openjdk-devel
 sudo curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum -y install jenkins
+sudo yum -y install jenkins git
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 sudo firewall-cmd --permanent --new-service=jenkins
